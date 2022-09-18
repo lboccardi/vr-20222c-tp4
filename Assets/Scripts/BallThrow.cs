18a063925b1467177e30affce4d79ca255c072ef
Luciano Boccardi <luciano.boccardi@mudafy.com>
2022-09-18 19:01:01 -0300

    added BallThrow

---
+++ Assets/Scripts/BallThrow.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallThrow : MonoBehaviour
+{
+    [SerializeField] private float _magnitude = 9F;
+
+    void Start()
+    {
+        Vector3 vector = new Vector3(1, 2, 0).normalized;
+        this.transform.GetComponent<Rigidbody>().AddForce(vector * _magnitude, ForceMode.Impulse);
+    }
+
+}
